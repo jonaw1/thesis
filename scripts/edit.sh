4ece671a45c3e2dd74d6684a2b1051346943d011
Jonathan Wagner <wagnerxjonathan@gmail.com>
2025-01-27 12:50:33 +0000

    add edit script

---
+++ scripts/edit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --time=01:00:00
+#SBATCH --output=example.out
+#SBATCH --error=error.out
+#SBATCH --mem=250GB
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=2
+
+# Specify the Conda environment name
+ENV_NAME="thesis_env"
+
+# Initialize Conda
+eval "$(conda shell.bash hook)"
+
+# Activate the Conda environment
+conda activate "$ENV_NAME"
+
+# Run Python code
+python3 src/edit.py
+
+# Deactivate the Conda environment
+conda deactivate
